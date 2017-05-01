1ae01e7d7396ac4239895a6522c59efc46ce9bb8
Chris Zempel <zempel@efeqdev.com>
2017-04-30 21:01:01 -0500

    More digest/watcher enhancements

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Progress Tracking
 * 4/29/17 - p 36, got to 'Checking for dirty values'
+* 4/30/17 - p 42, got to middle of trying to understand block of code at
+  top of page
--- src/scope.js
+++ src/scope.js
@@ -1,6 +1,8 @@
 'use strict';
 var _ = require("lodash");
 
+function initWatchVal() {};
+
 function Scope() {
   this.$$watchers = [];
 }
@@ -8,14 +10,24 @@ function Scope() {
 Scope.prototype.$watch = function(watchFn, listenerFn) {
   var watcher = {
     watchFn: watchFn,
-    listenerFn: listenerFn
+    listenerFn: listenerFn || function() {},
+    last: initWatchVal
   };
   this.$$watchers.push(watcher);
 };
 
 Scope.prototype.$digest = function() {
+  var self = this;
+  var newValue, oldValue;
   _.forEach(this.$$watchers, function(watcher) {
-    watcher.listenFn();
+    newValue = watcher.watchFn(self);
+    oldValue = watcher.last;
+    if (newValue !== oldValue) {
+      watcher.last = newValue;
+      watcher.listenerFn(newValue,
+                         (oldValue === initWatchVal ? newValue : oldValue),
+                         self);
+    }
   });
 };
 
