071b0808689f1e7d26f7d3cc3bc21e26582918e7
Chris Zempel <zempel@efeqdev.com>
2017-05-15 14:52:17 -0500

    Working function calls

--- src/parse.js
+++ src/parse.js
@@ -20,7 +20,7 @@ Lexer.prototype.lex = function(text) {
       this.readNumber();
     } else if (this.is('\'"')) {
       this.readString(this.ch);
-    } else if (this.is('[],{}:.')) {
+    } else if (this.is('[],{}:.()')) {
       this.tokens.push({
         text: this.ch
       });
@@ -163,6 +163,7 @@ AST.Identifier = 'Identifier';
 AST.ThisExpression = 'ThisExpression';
 AST.LocalsExpression = 'LocalsExpression';
 AST.MemberExpression = 'MemberExpression';
+AST.CallExpression = 'CallExpression';
 
 AST.prototype.ast = function(text) {
   this.tokens = this.lexer.lex(text);
@@ -185,7 +186,7 @@ AST.prototype.primary = function() {
     primary = this.constant();
   }
   var next;
-  while ((next = this.expect('.', '['))) {
+  while ((next = this.expect('.', '[', '('))) {
     if (next.text === '[') {
       primary = {
         type: AST.MemberExpression,
@@ -194,14 +195,21 @@ AST.prototype.primary = function() {
         computed: true
       };
       this.consume(']');
-    } else {
+    } else if (next.text === '.') {
       primary = {
         type: AST.MemberExpression,
         object: primary,
         property: this.identifier(),
         computed: false
       };
-    }
+    } else if (next.text === '(') {
+      primary = {
+        type: AST.CallExpression,
+        callee: primary,
+        arguments: this.parseArguments()
+      };
+      this.consume(')');
+    };
   }
   return primary;
 };
@@ -263,6 +271,15 @@ AST.prototype.consume = function(e) {
     throw 'Unexpected. Expecting: ' + e;
   }
   return token;
+};
+AST.prototype.parseArguments = function() {
+  var args = [];
+  if (!this.peek(')')) {
+    do {
+      args.push(this.primary());
+    } while (this.expect(','));
+  }
+  return args;
 };
 AST.prototype.constants = {
   'null':     {type: AST.Literal, value: null},
@@ -334,6 +351,12 @@ ASTCompiler.prototype.recurse = function(ast) {
     return intoId;
   case AST.LocalsExpression:
     return 'l';
+  case AST.CallExpression:
+    var callee = this.recurse(ast.callee);
+    var args = _.map(ast.arguments, _.bind(function(arg) {
+      return this.recurse(arg);
+    }, this));
+    return callee + '&&' + callee + '(' + args.join(',') + ')';
   }
 };
 
@@ -392,4 +415,3 @@ function parse(expr) {
 }
 
 module.exports = parse;
-
